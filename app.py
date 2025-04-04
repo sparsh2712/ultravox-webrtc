89cb4b650da991546d9c2e6141be931830466e63
sparsh2712 <sparshbadjate27@gmail.com>
2025-04-04 09:32:40 +0000

    dump

--- app.py
+++ app.py
@@ -40,4 +40,4 @@ def get_join_url():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+    app.run(debug=True, port=8080, ssl_context='adhoc')
